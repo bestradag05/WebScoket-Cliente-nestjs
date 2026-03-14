e4c5907b9b18a9a2ec76dbfd559915c578c8f3ee
bestradag05 <bestradag05@gmail.com>
2026-03-14 13:03:56 -0500

    Listando mensajes para todos los usuarios

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,19 @@ import { connectToServer } from './socket-client.ts';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Websocket - Client</h1>
-    <span>offline</span>
+    <span id="server-status">offline</span>
+    <ul id="clients-ul"></ul>
+
+    <form id="message-form">
+      <input id="message-input" type="text" placeholder="message" />
+    </form>
+
+
+    <h3>Messages</h3>
+    <ul id="messages-ul">
+      
+    </ul>
+
   </div>
 `
 
